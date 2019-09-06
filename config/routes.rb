48f5a11b3fa66f7ecbdb170968c621067842411e
gudimallaavinash0999 <gudimallaavinash0999@gmail.com>
2019-09-06 18:11:29 +0530

    mbo3

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'index',to:'courses#index'
+  root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
   get 'contact_us', to: 'pages#contact_us'
-  root 'logins#new'
+  get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
   resources :students
   post 'course_enroll', to: 'student_course#create'
-  get 'about',to:'pages#about'
+  get'about',to:'pages#about'
 end
